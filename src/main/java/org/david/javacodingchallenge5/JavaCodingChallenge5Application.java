7a3be3fed07c31e88832b12ec3618f8a3498198e
Alex David Frías <adavidfruiz@gmail.com>
2024-06-19 21:44:49 -0500

    feat: add project

---
+++ src/main/java/org/david/javacodingchallenge5/JavaCodingChallenge5Application.java
@@ -0,0 +1,13 @@
+package org.david.javacodingchallenge5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingChallenge5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodingChallenge5Application.class, args);
+    }
+
+}
